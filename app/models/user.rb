b6e700b155041d4cb2979943e3949f5ae9abc2b1
K.K <gokennsirou@yahoo.co.jp>
2020-01-06 20:45:10 +0900

    [Fix]Rubocopの自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
   has_secure_password
-  validates :name, presence:true, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 15 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, confirmation: true,
                     unless: :uid?,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, unless: :uid?, length: { minimum: 6 }, allow_nil:true
+  validates :password, presence: true, unless: :uid?, length: { minimum: 6 }, allow_nil: true
   
   class << self
     def digest(string)
@@ -36,6 +36,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
