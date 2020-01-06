74ed6ce0bdbf0c8b6bda5ce4427886ec71ddc032
K.K <gokennsirou@yahoo.co.jp>
2020-01-06 09:04:55 +0900

    [Add]現時点でのコントローラー、ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root    'homes#top'
+  get     '/about',     to: 'homes#about'
+  
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  delete  '/logout',    to: 'sessions#destroy'
+  get     '/auth/failure',             to: 'sessions#auth_failure'
+  get     '/auth/:provider/callback',  to: 'sessions#create'
+  
+  post    '/result',    to: 'searches#result'
+  post    '/pixabay',    to: 'searches#pixabay'
+
+  get     'communities/words'
+  get     'communities/questions'
+  get     'communities/ranking'
+  
+  resource :user, only: [:new, :create, :show, :update, :destroy]
+  resources :later_lists, only: [:create, :destroy]
+  resources :learns, controller: :learned_contents do
+    member do
+      get :question, :answer
+    end
+  end
+
+  namespace :admin do
+    get     '/login',     to: 'sessions#new'
+    post    '/login',     to: 'sessions#create'
+    delete  '/logout',    to: 'sessions#destroy'
+    resources :users, only: [:index, :destroy]
+    resources :learns, only: [:index, :show, :destroy], controller: :learned_contents
+  end
 end
---
+++ spec/controllers/homes_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe HomesController, type: :controller do
+
+  describe "GET #top" do
+    it "returns http success" do
+      get :top
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
