922027c866ca6549d450d88fd7e2a2cbba5e2402
K.K <gokennsirou@yahoo.co.jp>
2020-01-06 15:18:41 +0900

    [Add]ヘッダー、フッターを作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,14 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', function() {
+  $('.header-right__toggler--community').on('click', function () {
+    $('.community-menu').fadeToggle('fast');
+    return false;
+  });
+  $('.header-right__toggler--user').on('click', function () {
+    $('.user-menu').fadeToggle('fast');
+    return false;
+  });
+});
