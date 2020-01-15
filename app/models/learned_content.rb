cd34ba29f1583ee3dacc253deb6beaab8dcb7345
K.K <gokennsirou@yahoo.co.jp>
2020-01-15 12:25:16 +0900

    [Fix]カレンダーに復習日が過ぎた問題が表示されない問題を修正

--- app/models/learned_content.rb
+++ app/models/learned_content.rb
@@ -11,7 +11,7 @@ class LearnedContent < ApplicationRecord
   accepts_nested_attributes_for :questions
 
   scope :to_review_today, -> { where('till_next_review <= 0') }
-  scope :to_review_this_day, ->(date) { where('till_next_review = ?', (date - Time.zone.today).to_i) }
+  scope :to_review_this_day, ->(date) { where('till_next_review = ? AND till_next_review >= 1', (date - Time.zone.today).to_i) }
   scope :latest, -> { order(id: 'DESC') }
   scope :all_or_monthly, ->(monthly) do
     if monthly
