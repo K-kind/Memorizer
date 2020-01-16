921036e78411eb648ca6c3e13e93f9b0d9fced9f
K.K <gokennsirou@yahoo.co.jp>
2020-01-16 17:45:15 +0900

    [Fix]masterキーがない状態によるえらーを修正

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.add_delivery_method :ses,
                                        AWS::SES::Base,
-                                       access_key_id: Rails.application.credentials.ses[:access_key_id],
-                                       secret_access_key: Rails.application.credentials.ses[:secret_access_key],
+                                       access_key_id: Rails.application.credentials.dig(:ses, :access_key_id),
+                                       secret_access_key: Rails.application.credentials.dig(:ses, :secret_access_key),
                                        server: 'email.us-west-2.amazonaws.com'
