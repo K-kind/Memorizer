4c10276e189d50849cc7b38496ee92288133f485
K.K <gokennsirou@yahoo.co.jp>
2020-01-17 20:16:34 +0900

    [Add]学習数グラフを実装

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,4 +2,12 @@ class Calendar < ApplicationRecord
   has_many :learned_contents
   has_many :review_histories
   belongs_to :user
+
+  class << self
+    def learn_chart(new_or_review, day)
+      (day.prev_month .. day).to_a.map do |d|
+        [d, find_by(calendar_date: d)&.send(new_or_review)&.count]
+      end
+    end
+  end
 end
