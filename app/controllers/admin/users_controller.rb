c0d253d7642a23336330adbb8ebdc0bd39ca9747
K.K <gokennsirou@yahoo.co.jp>
2020-01-18 16:50:01 +0900

    [Fix]Rubocopの指摘を修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < AdminController
   before_action :logged_in_admin
-  before_action :set_user, only:[:show, :destroy]
+  before_action :set_user, only: [:show, :destroy]
 
   def index
     @users = User.all
