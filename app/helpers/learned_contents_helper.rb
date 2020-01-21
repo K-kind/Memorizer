5917d0f21b5c61eeabb76ac0965119671d399abc
K.K <gokennsirou@yahoo.co.jp>
2020-01-21 21:49:48 +0900

    [Update]復習showページのレイアウトを整える

--- app/helpers/learned_contents_helper.rb
+++ app/helpers/learned_contents_helper.rb
@@ -8,4 +8,14 @@ module LearnedContentsHelper
       'red'
     end
   end
+
+  def till_next_day(till_next_review)
+    if till_next_review <= 0
+      '0 days'
+    elsif till_next_review == 1
+      '1 day'
+    else
+      "#{till_next_review} days"
+    end
+  end
 end
