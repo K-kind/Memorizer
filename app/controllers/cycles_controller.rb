5c22967ded999b15452ebbdd04bff599c352855d
K.K <gokennsirou@yahoo.co.jp>
2020-01-25 14:30:38 +0900

    [Fix]Rubocopの指摘を修正

--- app/controllers/cycles_controller.rb
+++ app/controllers/cycles_controller.rb
@@ -2,9 +2,7 @@ class CyclesController < ApplicationController
   before_action :logged_in_user
 
   def set
-    if current_user.update(cycle_params)
-      @message = '復習サイクルを更新しました。'
-    end
+    @message = '復習サイクルを更新しました。' if current_user.update(cycle_params)
   end
 
   def new
@@ -21,7 +19,7 @@ class CyclesController < ApplicationController
     @message = '最後のサイクルを削除しました。'
     render 'set'
   end
-  
+
   def default
     [1, 7, 16, 35, 62].each_with_index do |cycle, index|
       new_cycle = current_user.cycles.find_by(times: index)
