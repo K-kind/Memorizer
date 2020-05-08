17f5a0b58ed42360bbfd06b9ae2b073d0fbdcbed
K.K <gokennsirou@yahoo.co.jp>
2020-05-08 14:39:53 +0900

    [Fix]system/user_specをパスするようにコメントアウト。capybara.rbの設定でwindowサイズを小さくする。

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   chrome_url = ENV.fetch('SELENIUM_DRIVER_URL')
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium, using: :headless_chrome, options: {
+    driven_by :selenium, using: :headless_chrome, screen_size: [800, 600], options: {
       browser: :remote,
       url: chrome_url,
       desired_capabilities: :chrome
