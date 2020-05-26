d3c65201ae2b9b5eb85da03695561f3c05e44c7f
K.K <gokennsirou@yahoo.co.jp>
2020-05-26 16:27:56 +0900

    [Add] release notes crud in admin

--- app/controllers/admin/release_notes_controller.rb
+++ app/controllers/admin/release_notes_controller.rb
@@ -2,28 +2,40 @@ class Admin::ReleaseNotesController < AdminController
   before_action :set_release_note, only: [:show, :update, :destroy]
 
   def index
-    @release_notes = ReleaseNote.order(created_at: :desc).page(params[:page])
+    @release_notes = ReleaseNote.order(release_date: :desc).page(params[:page])
     @new_release = ReleaseNote.new
   end
 
   def show; end
 
   def create
-    @new_release = ReleaseNote.new(content_params)
+    @new_release = ReleaseNote.new
+    @new_release = ReleaseNote.new(release_note_params)
+    if @new_release.save
+      redirect_to admin_release_note_url(@new_release)
+    else
+      @release_notes = ReleaseNote.order(created_at: :desc).page(params[:page])
+      render 'index'
+    end
   end
 
   def update
-
+    if @release_note.update(release_note_params)
+      redirect_to admin_release_note_url(@release_note)
+    else
+      render 'show'
+    end
   end
 
   def destroy
-
+    @release_note.destroy
+    redirect_to admin_release_notes_url
   end
 
   private
 
   def release_note_params
-    params.requre(:release_note).permit(:content, :version, :month, :date)
+    params.require(:release_note).permit(:content, :version, :year, :month, :day)
   end
 
   def set_release_note
--- app/models/release_note.rb
+++ app/models/release_note.rb
@@ -1,2 +1,26 @@
 class ReleaseNote < ApplicationRecord
+  VALID_VERSION = /[\d]+.[\d]+.[\d]+/.freeze
+  validates :content, length: { maximum: 3000 }, presence: true
+  validates :version, presence: true, format: { with: VALID_VERSION }
+  validates :release_date, presence: true
+
+  attr_accessor :year
+  attr_accessor :month
+  attr_accessor :day
+  before_validation :set_release_date
+  after_find :set_date_variables
+
+  private
+
+  def set_release_date
+    self.release_date = Date.new(year.to_i, month.to_i, day.to_i)
+  rescue ArgumentError
+    self.release_date = nil
+  end
+
+  def set_date_variables
+    @year = release_date&.year
+    @month = release_date&.month
+    @day = release_date&.day
+  end
 end
