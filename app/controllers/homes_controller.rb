e3b13c612d13f89aa9ff8fcf824b33f8a05c649f
K.K <gokennsirou@yahoo.co.jp>
2020-05-27 23:58:57 +0900

    [Update] SQL performance in the top page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -9,13 +9,44 @@ class HomesController < ApplicationController
   def top
     current_user.calendars.find_or_create_by!(calendar_date: Time.zone.today)
     @notice = Notice.last
-    @learned_contents_today = learned_contents_today
     @reviewed_count_today = reviewed_count_today
-    @contents_to_review_today = contents_to_review_today
+    @learned_contents_today =
+      current_user.calendars
+                  .find_by(calendar_date: Time.zone.today)
+                  .learned_contents
+                  .includes([:word_definition,
+                             :questions,
+                             {related_words: :word_definition}])
+    @contents_to_review_today =
+      current_user.learned_contents
+                  .to_review_today
+                  .includes([:word_definition,
+                             :questions,
+                             {related_words: :word_definition}])
   end
 
   def calendar
-    @calendars = current_user.calendars.a_month_old
+    @calendars =
+      current_user.calendars
+                  .a_month_old
+                  .left_joins(:learned_contents)
+                  .joins(
+                    'LEFT OUTER JOIN learned_contents
+                    AS to_do_contents
+                    ON to_do_contents.review_date =
+                    calendars.calendar_date'
+                  )
+                  .left_joins(:review_histories)
+                  .select(
+                    'calendars.id,
+                    calendars.calendar_date,
+                    COUNT(distinct learned_contents.id)
+                    AS contents_count,
+                    COUNT(distinct to_do_contents.id)
+                    AS to_do_count,
+                    COUNT(distinct review_histories.id)
+                    AS reviews_count'
+                  ).group(:id)
   end
 
   def about
--- app/views/homes/calendar.json.jbuilder
+++ app/views/homes/calendar.json.jbuilder
@@ -3,15 +3,18 @@ json.array!(@calendars) do |calendar|
   today = Time.zone.today
 
   if date < today
-    new_count = calendar.learned_contents.count
-    review_count = calendar.review_histories.count
+    new_count = calendar.contents_count
+    review_count = calendar.reviews_count
     title = "Learn: #{new_count}\nReview: #{review_count}" \
       if new_count != 0 || review_count != 0
   elsif date == today
-    title = "To do: #{current_user.learned_contents.to_review_today.count}\nLearn: #{calendar.learned_contents.count}\nReview: #{calendar.review_histories.count}"
+    to_do = current_user.learned_contents.to_review_today.count
+    learn = calendar.contents_count
+    review = calendar.reviews_count
+    title = "To do: #{to_do}\nLearn: #{learn}\nReview: #{review}"
   else
-    review_count = current_user.learned_contents.to_review_this_day(calendar.calendar_date).count
-    title = "To do: #{review_count}" unless review_count.zero?
+    to_do_count = calendar.to_do_count
+    title = "To do: #{to_do_count}" unless to_do_count.zero?
   end
   next unless title
 
