756b9b2340160ecbfeadc4e3fc75138a56a854f1
K.K <gokennsirou@yahoo.co.jp>
2020-05-29 23:38:43 +0900

    [Fix] sidekiq config indent[skip ci]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,23 @@
 Sidekiq.configure_server do |config|
   case Rails.env
-    when 'production' then
-      redis_conn = proc {
-        Redis.new(host: ENV['REDIS_URL'], port: 6379, db: 2)
-      }
-      config.redis = ConnectionPool.new(size: 27, &redis_conn)
-    else
-      config.redis = { url: ENV['REDIS_URL'] }
+  when 'production'
+    redis_conn = proc {
+      Redis.new(host: ENV['REDIS_URL'], port: 6379, db: 2)
+    }
+    config.redis = ConnectionPool.new(size: 27, &redis_conn)
+  else
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 end
 
 Sidekiq.configure_client do |config|
   case Rails.env
-    when 'production' then
-      redis_conn = proc {
-        Redis.new(host: ENV['REDIS_URL'], port: 6379, db: 2)
-      }
-      config.redis = ConnectionPool.new(size: 27, &redis_conn)
-    else
-      config.redis = { url: ENV['REDIS_URL'] }
+  when 'production'
+    redis_conn = proc {
+      Redis.new(host: ENV['REDIS_URL'], port: 6379, db: 2)
+    }
+    config.redis = ConnectionPool.new(size: 27, &redis_conn)
+  else
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 end
